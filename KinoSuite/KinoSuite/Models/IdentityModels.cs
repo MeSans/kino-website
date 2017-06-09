da7343a1336978fdde0c154e0ab5efeeb3dae6d0
Patriks <patriks.misans@gmail.com>
2017-06-09 19:04:36 +0300

    hosting added

--- KinoSuite/KinoSuite/Models/IdentityModels.cs
+++ KinoSuite/KinoSuite/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace KinoSuite.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("KinoDB", throwIfV1Schema: false)
         {
         }
 
